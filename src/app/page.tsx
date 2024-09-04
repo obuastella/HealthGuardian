b022dc997340ded3c0b17d67b968cb76365e2727
Obua Stella <stellaobua123@gmail.com>
2024-09-04 20:28:18 +0100

    added shacn button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1 className="text-orange-600">Hello world</h1>;
+  return (
+    <h1 className="text-orange-600">
+      Hello world
+      <Button>Click me i am shadcn button</Button>
+    </h1>
+  );
 }
