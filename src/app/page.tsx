6016fd4fdfacce899b9ab8201fcbdf0f044f37cf
Obua Stella <stellaobua123@gmail.com>
2024-09-05 22:17:33 +0000

    working on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <h1 className="text-orange-600">
-      Hello world
-      <Button>Click me i am shadcn button</Button>
+      landing page
     </h1>
   );
 }
